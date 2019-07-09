1dab35d669e365e398f439d1e8d37d4a13ef2a35
vicky <s353397@sinyi.com.tw>
2019-07-09 16:35:22 +0800

    Updates

---
+++ precache-manifest.e282c4cb854477f70a389b38d79f46fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5074ae7f13ef6129a63b586bb312b173",
+    "url": "/pomodoroTechnique/index.html"
+  },
+  {
+    "revision": "4c0e8e90b1b679b1ba53",
+    "url": "/pomodoroTechnique/static/css/main.7a52c41b.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/pomodoroTechnique/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "4c0e8e90b1b679b1ba53",
+    "url": "/pomodoroTechnique/static/js/main.67458a4f.chunk.js"
+  },
+  {
+    "revision": "108923ffba059528ec7e",
+    "url": "/pomodoroTechnique/static/js/runtime~main.563cd2c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/theApp/precache-manifest.c645e5739ad7bb1f6f1a8c2ce582cf63.js"
+  "/pomodoroTechnique/precache-manifest.e282c4cb854477f70a389b38d79f46fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/theApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pomodoroTechnique/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
