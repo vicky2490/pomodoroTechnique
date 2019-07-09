3c09b35c2a60619a569da29e469e7bff4aada067
vicky <s353397@sinyi.com.tw>
2019-07-09 16:24:47 +0800

    實作main page中心圓的部分

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="wrap">
+    <div className="flex-container">
+      <div className="left-content">
+      </div>
+ 
+      <div className="right-content">
+        <div className="outside-circle">
+        <div className="middle-circle">
+          <div className="work-circle">
+            <div className="material-icons play-arrow">play_arrow</div>
+          </div>
+          <div className="little-circle"></div>
+        </div>
+        </div>
+      </div>
     </div>
+    // </div>
   );
 }
 
